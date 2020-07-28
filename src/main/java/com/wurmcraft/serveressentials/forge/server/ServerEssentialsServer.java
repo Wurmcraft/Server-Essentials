6869721325eaef077f551c653742da44f7a16cbd
Wurmatron <wurmatron@gmail.com>
2020-07-28 12:53:34 -0400

    Added Module / Command Annotation Loader

--- src/main/java/com/wurmcraft/serveressentials/forge/server/ServerEssentialsServer.java
+++ src/main/java/com/wurmcraft/serveressentials/forge/server/ServerEssentialsServer.java
@@ -1,5 +1,7 @@
 package com.wurmcraft.serveressentials.forge.server;
 
+import com.wurmcraft.serveressentials.forge.server.loader.CommandLoader;
+import com.wurmcraft.serveressentials.forge.server.loader.ModuleLoader;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
@@ -8,7 +10,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid = Global.MODID, name = Global.NAME, serverSideOnly = true)
+@Mod(modid = Global.MODID, name = Global.NAME, serverSideOnly = true, acceptableRemoteVersions = "*")
 public class ServerEssentialsServer {
 
   public static final Logger LOGGER = LogManager.getLogger(Global.NAME);
@@ -16,11 +18,13 @@ public class ServerEssentialsServer {
   @EventHandler
   public void preInit(FMLPreInitializationEvent e) {
     LOGGER.info("Pre-Init has Started");
+    ModuleLoader.setupModule();
   }
 
   @EventHandler
   public void init(FMLInitializationEvent e) {
     LOGGER.info("Init has Started");
+    CommandLoader.setupCommands();
   }
 
   @EventHandler
