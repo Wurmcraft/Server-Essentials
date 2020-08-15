ba62b4b7594a20c77fdb537cf6133e9f7ba69ed9
Wurmatron <wurmatron@gmail.com>
2020-08-15 17:17:34 -0400

    Updated BuildScript Fixed FTBUtils Module Loading

--- src/main/java/com/wurmcraft/serveressentials/forge/modules/ftbutils/FTBUtilsModule.java
+++ src/main/java/com/wurmcraft/serveressentials/forge/modules/ftbutils/FTBUtilsModule.java
@@ -1,11 +1,13 @@
 package com.wurmcraft.serveressentials.forge.modules.ftbutils;
 
+import com.wurmcraft.serveressentials.forge.api.module.Module;
 import com.wurmcraft.serveressentials.forge.modules.ftbutils.event.FTBUtilsEvents;
 import com.wurmcraft.serveressentials.forge.server.ServerEssentialsServer;
 import java.io.File;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Loader;
 
+@Module(name = "FtbUtils")
 public class FTBUtilsModule {
 
   public static File PLAYER_RANKS = new File("local" + File.separator + "ftbutilities" + File.separator + "players.txt");
@@ -20,5 +22,11 @@ public class FTBUtilsModule {
 
   public void finalizeModule() {
 
+  }
+
+  public void reloadModule() {
+    ServerEssentialsServer.isUpdateInProgress = true;
+    initSetup();
+    ServerEssentialsServer.isUpdateInProgress = false;
   }
 }
