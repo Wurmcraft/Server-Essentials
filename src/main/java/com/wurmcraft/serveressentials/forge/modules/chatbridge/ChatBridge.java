bccee187516705940faa76bbd074e15616b89bee
Wurmatron <wurmatron@gmail.com>
2020-10-19 20:57:01 -0400

    Enabled Reloading ChatBridge

--- src/main/java/com/wurmcraft/serveressentials/forge/modules/chatbridge/ChatBridge.java
+++ src/main/java/com/wurmcraft/serveressentials/forge/modules/chatbridge/ChatBridge.java
@@ -2,6 +2,7 @@ package com.wurmcraft.serveressentials.forge.modules.chatbridge;
 
 import com.wurmcraft.serveressentials.forge.api.module.Module;
 import com.wurmcraft.serveressentials.forge.modules.chatbridge.event.ChatSocketEvents;
+import com.wurmcraft.serveressentials.forge.server.ServerEssentialsServer;
 
 @Module(name = "ChatBridge", moduleDependencies = {"General", "Language"})
 public class ChatBridge {
@@ -15,5 +16,8 @@ public class ChatBridge {
   }
 
   public void reloadModule() {
+    ServerEssentialsServer.isUpdateInProgress = true;
+    ChatSocketEvents.setup();
+    ServerEssentialsServer.isUpdateInProgress = false;
   }
 }
