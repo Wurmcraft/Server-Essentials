d59d93dc1edd68b96e961939c2837b022faf5baf
Wurmatron <wurmatron@gmail.com>
2021-03-28 12:16:03 -0400

    Work on Rest API

---
+++ Backend-Rest/src/main/java/io/wurmatron/serveressentials/ServerEssentialsRest.java
@@ -0,0 +1,13 @@
+package io.wurmatron.serveressentials;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ServerEssentialsRest {
+
+    public static final Logger LOG = LoggerFactory.getLogger(ServerEssentialsRest.class);
+
+    public static void main(String[] args) {
+
+    }
+}
