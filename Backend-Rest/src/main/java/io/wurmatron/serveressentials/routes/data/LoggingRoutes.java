baa7dfae67745d07d3efd80586df6b019b9fd6ec
Wurmatron <wurmatron@gmail.com>
2021-05-19 01:35:30 -0400

    Added /logging Routes Template and Tests

---
+++ Backend-Rest/src/main/java/io/wurmatron/serveressentials/routes/data/LoggingRoutes.java
@@ -0,0 +1,103 @@
+package io.wurmatron.serveressentials.routes.data;
+
+
+import io.javalin.http.Handler;
+import io.javalin.plugin.openapi.annotations.*;
+import io.wurmatron.serveressentials.models.LogEntry;
+import io.wurmatron.serveressentials.models.MessageResponse;
+import io.wurmatron.serveressentials.routes.Route;
+
+public class LoggingRoutes {
+
+    @OpenApi(
+            summary = "Create a new logging event",
+            description = "Create a new logging event",
+            tags = {"Logging"},
+            headers = {@OpenApiParam(name = "Authorization", description = "Authorization Token to used for authentication within the rest API", required = true)},
+            requestBody = @OpenApiRequestBody(content = {@OpenApiContent(from = LogEntry.class)}, required = true, description = "Information about the log event"),
+            responses = {
+                    @OpenApiResponse(status = "201", content = {@OpenApiContent(from = LogEntry.class)}, description = "Log Entry has been created successfully,"),
+                    @OpenApiResponse(status = "400", content = {@OpenApiContent(from = MessageResponse[].class)}, description = "One or more of the provided values, has failed to validate!"),
+                    @OpenApiResponse(status = "401", content = {@OpenApiContent(from = MessageResponse.class)}, description = "You are missing an authorization token"),
+                    @OpenApiResponse(status = "403", content = {@OpenApiContent(from = MessageResponse.class)}, description = "Forbidden, Your provided auth token does not have permission to do this"),
+                    @OpenApiResponse(status = "422", content = {@OpenApiContent(from = MessageResponse.class)}, description = "Unable to process, due to invalid format / json"),
+                    @OpenApiResponse(status = "500", content = {@OpenApiContent(from = MessageResponse.class)}, description = "The server has encountered an error, please contact the server's admin to check the logs")
+            }
+    )
+    // TODO Implement
+    @Route(path = "logging", method = "POST", roles = {Route.RestRoles.USER, Route.RestRoles.SERVER, Route.RestRoles.DEV})
+    public static Handler create = ctx -> {
+
+    };
+
+    @OpenApi(
+            summary = "Filter the log entries based on your query",
+            description = "Filter the log entries based on your query",
+            tags = {"Logging"},
+            headers = {@OpenApiParam(name = "Authorization", description = "Authorization Token to used for authentication within the rest API", required = true)},
+            queryParams = {
+                    @OpenApiParam(name = "server-id", description = "ID of the server where this event took place"),
+                    @OpenApiParam(name = "action", description = "Type of action that has taken place"),
+                    @OpenApiParam(name = "uuid", description = "UUID of the user / account that caused this event"),
+                    @OpenApiParam(name = "x", description = "X Position that this event took place", type = Integer.class),
+                    @OpenApiParam(name = "y", description = "Y Position that this event took place", type = Integer.class),
+                    @OpenApiParam(name = "z", description = "Z Position that this event took place", type = Integer.class),
+                    @OpenApiParam(name = "dim", description = "Dimension that this event took place", type = Integer.class)
+            },
+            responses = {
+                    @OpenApiResponse(status = "200", content = {@OpenApiContent(from = LogEntry[].class)}, description = "Log Entry's that fit into your required filters (query)"),
+                    @OpenApiResponse(status = "400", content = {@OpenApiContent(from = MessageResponse[].class)}, description = "One or more of the provided values, has failed to validate!"),
+                    @OpenApiResponse(status = "401", content = {@OpenApiContent(from = MessageResponse.class)}, description = "You are missing an authorization token"),
+                    @OpenApiResponse(status = "403", content = {@OpenApiContent(from = MessageResponse.class)}, description = "Forbidden, Your provided auth token does not have permission to do this"),
+                    @OpenApiResponse(status = "422", content = {@OpenApiContent(from = MessageResponse.class)}, description = "Unable to process, due to invalid format / json"),
+                    @OpenApiResponse(status = "500", content = {@OpenApiContent(from = MessageResponse.class)}, description = "The server has encountered an error, please contact the server's admin to check the logs")
+            }
+    )
+    // TODO Implement
+    @Route(path = "logging", method = "GET")
+    public static Handler get = ctx -> {
+
+    };
+
+    @OpenApi(
+            summary = "Update an existing log event",
+            description = "Update an existing log event",
+            tags = {"Logging"},
+            headers = {@OpenApiParam(name = "Authorization", description = "Authorization Token to used for authentication within the rest API", required = true)},
+            requestBody = @OpenApiRequestBody(content = {@OpenApiContent(from = LogEntry.class)}, required = true, description = "Information about the log event"),
+            responses = {
+                    @OpenApiResponse(status = "200", content = {@OpenApiContent(from = LogEntry.class)}, description = "Log Entry has been successfully updated"),
+                    @OpenApiResponse(status = "400", content = {@OpenApiContent(from = MessageResponse[].class)}, description = "One or more of the provided values, has failed to validate!"),
+                    @OpenApiResponse(status = "401", content = {@OpenApiContent(from = MessageResponse.class)}, description = "You are missing an authorization token"),
+                    @OpenApiResponse(status = "403", content = {@OpenApiContent(from = MessageResponse.class)}, description = "Forbidden, Your provided auth token does not have permission to do this"),
+                    @OpenApiResponse(status = "422", content = {@OpenApiContent(from = MessageResponse.class)}, description = "Unable to process, due to invalid format / json"),
+                    @OpenApiResponse(status = "500", content = {@OpenApiContent(from = MessageResponse.class)}, description = "The server has encountered an error, please contact the server's admin to check the logs")
+            }
+    )
+    // TODO Implement
+    @Route(path = "logging", method = "PUT", roles = {Route.RestRoles.USER, Route.RestRoles.SERVER, Route.RestRoles.DEV})
+    public static Handler override = ctx -> {
+
+    };
+
+    @OpenApi(
+            summary = "Remove an existing log event",
+            description = "Remove an existing log event",
+            tags = {"Logging"},
+            headers = {@OpenApiParam(name = "Authorization", description = "Authorization Token to used for authentication within the rest API", required = true)},
+            requestBody = @OpenApiRequestBody(content = {@OpenApiContent(from = LogEntry.class)}, required = true, description = "The event entry you want to remove"),
+            responses = {
+                    @OpenApiResponse(status = "200", content = {@OpenApiContent(from = LogEntry.class)}, description = "Log Entry has been successfully deleted"),
+                    @OpenApiResponse(status = "400", content = {@OpenApiContent(from = MessageResponse[].class)}, description = "One or more of the provided values, has failed to validate!"),
+                    @OpenApiResponse(status = "401", content = {@OpenApiContent(from = MessageResponse.class)}, description = "You are missing an authorization token"),
+                    @OpenApiResponse(status = "403", content = {@OpenApiContent(from = MessageResponse.class)}, description = "Forbidden, Your provided auth token does not have permission to do this"),
+                    @OpenApiResponse(status = "422", content = {@OpenApiContent(from = MessageResponse.class)}, description = "Unable to process, due to invalid format / json"),
+                    @OpenApiResponse(status = "500", content = {@OpenApiContent(from = MessageResponse.class)}, description = "The server has encountered an error, please contact the server's admin to check the logs")
+            }
+    )
+    // TODO Implement
+    @Route(path = "logging", method = "DELETE", roles = {Route.RestRoles.USER, Route.RestRoles.SERVER, Route.RestRoles.DEV})
+    public static Handler delete = ctx -> {
+
+    };
+}
---
+++ Backend-Rest/src/test/java/io/wurmatron/serveressentials/tests/routes/TestLoggingRoutes.java
@@ -0,0 +1,86 @@
+package io.wurmatron.serveressentials.tests.routes;
+
+import io.wurmatron.serveressentials.ServerEssentialsRest;
+import io.wurmatron.serveressentials.models.LogEntry;
+import io.wurmatron.serveressentials.tests.sql.TestLogging;
+import io.wurmatron.serveressentials.tests.utils.HTTPRequests;
+import org.junit.jupiter.api.*;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+
+public class TestLoggingRoutes {
+
+    @BeforeAll
+    public static void setup() throws IOException, SQLException {
+        ServerEssentialsRest.main(new String[]{});
+        ServerEssentialsRest.config.server.host = "localhost";
+        HTTPRequests.BASE_URL = "http://" + ServerEssentialsRest.config.server.host + ":" + ServerEssentialsRest.config.server.port + "/";
+        // TODO Add Authentication or force config.testing when running tests
+    }
+
+    @Test
+    @Order(1)
+    public void testAddLoggingEvent() throws IOException {
+        LogEntry newEntry = HTTPRequests.postWithReturn("logging", TestLogging.TEST_ENTRY, LogEntry.class);
+        assertNotNull(newEntry, "Log Entry is not null");
+        // Check if the log entry was created successfully
+        LogEntry[] entries = HTTPRequests.get("logging?action=" + TestLogging.TEST_ENTRY.actionType, LogEntry[].class);
+        boolean exists = false;
+        for (LogEntry entry : entries)
+            if (TestLogging.TEST_ENTRY.equals(entry)) {
+                exists = true;
+                break;
+            }
+        assertTrue(exists, "Log Entry was added successfully");
+    }
+
+    @Test
+    @Order(2)
+    public void testGetLogEvent() throws IOException {
+        LogEntry[] entries = HTTPRequests.get("logging?action=" + TestLogging.TEST_ENTRY.actionType, LogEntry[].class);
+        boolean exists = false;
+        for (LogEntry entry : entries)
+            if (TestLogging.TEST_ENTRY.equals(entry)) {
+                exists = true;
+                break;
+            }
+        assertTrue(exists, "Log Entry was added successfully");
+    }
+
+    @Test
+    @Order(2)
+    public void testUpdateLogEvent() throws IOException {
+        TestLogging.TEST_ENTRY.x = 50000;
+        HTTPRequests.put("logging", TestLogging.TEST_ENTRY);
+        // Check if the log entry was updated successfully
+        LogEntry[] entries = HTTPRequests.get("logging?action=" + TestLogging.TEST_ENTRY.actionType, LogEntry[].class);
+        boolean exists = false;
+        for (LogEntry entry : entries)
+            if (TestLogging.TEST_ENTRY.equals(entry)) {
+                exists = true;
+                break;
+            }
+        assertTrue(exists, "Log Entry was updated successfully");
+    }
+
+    @Test
+    @Order(3)
+    public void testDeleteLogEvent() throws IOException {
+        LogEntry deletedEntry = HTTPRequests.deleteWithReturn("logging", TestLogging.TEST_ENTRY, LogEntry.class);
+        assertNotNull(deletedEntry, "Entry was deleted successfully");
+        // Make sure the log event was deleted successfully
+        LogEntry[] entries = HTTPRequests.get("logging?action=" + TestLogging.TEST_ENTRY.actionType, LogEntry[].class);
+        boolean exists = false;
+        for (LogEntry entry : entries)
+            if (TestLogging.TEST_ENTRY.equals(entry)) {
+                exists = true;
+                break;
+            }
+        assertFalse(exists, "Log Entry was deleted successfully");
+    }
+}
