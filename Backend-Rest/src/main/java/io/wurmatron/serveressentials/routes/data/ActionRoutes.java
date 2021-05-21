514de6170c55146bb65fa115172008f05e228b1a
Wurmatron <wurmatron@gmail.com>
2021-05-21 19:40:33 -0400

    Added /action Routes Template and Tests

---
+++ Backend-Rest/src/main/java/io/wurmatron/serveressentials/routes/data/ActionRoutes.java
@@ -0,0 +1,100 @@
+package io.wurmatron.serveressentials.routes.data;
+
+import io.javalin.http.Handler;
+import io.javalin.plugin.openapi.annotations.*;
+import io.wurmatron.serveressentials.models.Action;
+import io.wurmatron.serveressentials.models.MessageResponse;
+import io.wurmatron.serveressentials.routes.Route;
+
+public class ActionRoutes {
+
+    @OpenApi(
+            summary = "Create / log a new action",
+            description = "Create / log a new action",
+            tags = {"Action"},
+            requestBody = @OpenApiRequestBody(content = {@OpenApiContent(from = Action.class)}, required = true, description = "Information about the action"),
+            responses = {
+                    @OpenApiResponse(status = "201", content = {@OpenApiContent(from = Action.class)}, description = "Action instance of to created / logged"),
+                    @OpenApiResponse(status = "400", content = {@OpenApiContent(from = MessageResponse[].class)}, description = "One or more of the provided values, has failed to validate!"),
+                    @OpenApiResponse(status = "401", content = {@OpenApiContent(from = MessageResponse.class)}, description = "You are missing an authorization token"),
+                    @OpenApiResponse(status = "403", content = {@OpenApiContent(from = MessageResponse.class)}, description = "Forbidden, Your provided auth token does not have permission to do this"),
+                    @OpenApiResponse(status = "409", content = {@OpenApiContent(from = MessageResponse.class)}, description = "Action already exists"),
+                    @OpenApiResponse(status = "422", content = {@OpenApiContent(from = MessageResponse.class)}, description = "Unable to process, due to invalid format / json"),
+                    @OpenApiResponse(status = "500", content = {@OpenApiContent(from = MessageResponse.class)}, description = "The server has encountered an error, please contact the server's admin to check the logs")
+            }
+    )
+    // TODO Implement
+    @Route(path = "action", method = "POST", roles = {Route.RestRoles.USER, Route.RestRoles.SERVER, Route.RestRoles.DEV})
+    public static Handler create = ctx -> {
+
+    };
+
+    @OpenApi(
+            summary = "Get a list / array of action's matching the requested criteria, (query)",
+            description = "Get a list / array of action's matching the requested criteria, (query)",
+            tags = {"Action"},
+            queryParams = {
+                    @OpenApiParam(name = "related-id", description = "Server ID or discord ID related to the provided action"),
+                    @OpenApiParam(name = "host", description = "Type that the related-id is related to, 'Minecraft', 'Discord'"),
+                    @OpenApiParam(name = "action", description = "Type of action that occurred"),
+                    @OpenApiParam(name = "min-timestamp", description = "Starting Time / Earliest time this action can have occurred"),
+                    @OpenApiParam(name = "max-timestamp", description = "Last time this action can has occurred"),
+            },
+            responses = {
+                    @OpenApiResponse(status = "200", content = {@OpenApiContent(from = Action[].class)}, description = "List of actions based on your requested filters (query)"),
+                    @OpenApiResponse(status = "400", content = {@OpenApiContent(from = MessageResponse[].class)}, description = "One or more of the provided values, has failed to validate!"),
+                    @OpenApiResponse(status = "401", content = {@OpenApiContent(from = MessageResponse.class)}, description = "You are missing an authorization token"),
+                    @OpenApiResponse(status = "403", content = {@OpenApiContent(from = MessageResponse.class)}, description = "Forbidden, Your provided auth token does not have permission to do this"),
+                    @OpenApiResponse(status = "404", content = {@OpenApiContent(from = MessageResponse.class)}, description = "Action does not exist"),
+                    @OpenApiResponse(status = "422", content = {@OpenApiContent(from = MessageResponse.class)}, description = "Unable to process, due to invalid format / json"),
+                    @OpenApiResponse(status = "500", content = {@OpenApiContent(from = MessageResponse.class)}, description = "The server has encountered an error, please contact the server's admin to check the logs")
+            }
+    )
+    // TODO Implement
+    @Route(path = "action", method = "GET")
+    public static Handler get = ctx -> {
+
+    };
+
+    @OpenApi(
+            summary = "Update an existing action",
+            description = "Update an existing action",
+            tags = {"Action"},
+            requestBody = @OpenApiRequestBody(content = {@OpenApiContent(from = Action.class)}, required = true, description = "Information about the action"),
+            responses = {
+                    @OpenApiResponse(status = "200", content = {@OpenApiContent(from = Action.class)}, description = "Updated action instance is returned"),
+                    @OpenApiResponse(status = "400", content = {@OpenApiContent(from = MessageResponse[].class)}, description = "One or more of the provided values, has failed to validate!"),
+                    @OpenApiResponse(status = "401", content = {@OpenApiContent(from = MessageResponse.class)}, description = "You are missing an authorization token"),
+                    @OpenApiResponse(status = "403", content = {@OpenApiContent(from = MessageResponse.class)}, description = "Forbidden, Your provided auth token does not have permission to do this"),
+                    @OpenApiResponse(status = "404", content = {@OpenApiContent(from = MessageResponse.class)}, description = "Action does not exist"),
+                    @OpenApiResponse(status = "422", content = {@OpenApiContent(from = MessageResponse.class)}, description = "Unable to process, due to invalid format / json"),
+                    @OpenApiResponse(status = "500", content = {@OpenApiContent(from = MessageResponse.class)}, description = "The server has encountered an error, please contact the server's admin to check the logs")
+            }
+    )
+    // TODO implement
+    @Route(path = "action", method = "PUT")
+    public static Handler update = ctx -> {
+
+    };
+
+    @OpenApi(
+            summary = "Delete an existing action",
+            description = "Delete an existing action",
+            tags = {"Action"},
+            requestBody = @OpenApiRequestBody(content = {@OpenApiContent(from = Action.class)}, required = true, description = "Information about the action"),
+            responses = {
+                    @OpenApiResponse(status = "200", content = {@OpenApiContent(from = Action.class)}, description = "Deleted action is returned"),
+                    @OpenApiResponse(status = "400", content = {@OpenApiContent(from = MessageResponse[].class)}, description = "One or more of the provided values, has failed to validate!"),
+                    @OpenApiResponse(status = "401", content = {@OpenApiContent(from = MessageResponse.class)}, description = "You are missing an authorization token"),
+                    @OpenApiResponse(status = "403", content = {@OpenApiContent(from = MessageResponse.class)}, description = "Forbidden, Your provided auth token does not have permission to do this"),
+                    @OpenApiResponse(status = "404", content = {@OpenApiContent(from = MessageResponse.class)}, description = "Action does not exist"),
+                    @OpenApiResponse(status = "422", content = {@OpenApiContent(from = MessageResponse.class)}, description = "Unable to process, due to invalid format / json"),
+                    @OpenApiResponse(status = "500", content = {@OpenApiContent(from = MessageResponse.class)}, description = "The server has encountered an error, please contact the server's admin to check the logs")
+            }
+    )
+    // TODO Implement
+    @Route(path = "action", method = "DELETE")
+    public static Handler delete = ctx -> {
+
+    };
+}
---
+++ Backend-Rest/src/test/java/io/wurmatron/serveressentials/tests/routes/TestActionRoutes.java
@@ -0,0 +1,84 @@
+package io.wurmatron.serveressentials.tests.routes;
+
+import io.wurmatron.serveressentials.ServerEssentialsRest;
+import io.wurmatron.serveressentials.models.Action;
+import io.wurmatron.serveressentials.tests.sql.TestActions;
+import io.wurmatron.serveressentials.tests.utils.HTTPRequests;
+import org.junit.jupiter.api.*;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class TestActionRoutes {
+
+    @BeforeAll
+    public static void setup() throws IOException, SQLException {
+        ServerEssentialsRest.main(new String[]{});
+        HTTPRequests.BASE_URL = "http://" + ServerEssentialsRest.config.server.host + ":" + ServerEssentialsRest.config.server.port + "/";
+        // TODO Add Authentication or force config.testing when running tests
+    }
+
+    @Test
+    @Order(1)
+    public void testAddAction() throws IOException {
+        Action addedAction = HTTPRequests.postWithReturn("action", TestActions.TEST_ACTION, Action.class);
+        assertNotNull(addedAction, "Action was created successfully");
+        // Make sure the action was added successfully
+        Action[] actions = HTTPRequests.get("action?action=" + addedAction.action, Action[].class);
+        boolean exists = false;
+        for (Action action : actions)
+            if (TestActions.TEST_ACTION.equals(action)) {
+                exists = true;
+                break;
+            }
+        assertTrue(exists, "Action was added successfully");
+    }
+
+    @Test
+    @Order(2)
+    public void testGetAction() throws IOException {
+        Action[] actions = HTTPRequests.get("action?host=" + TestActions.TEST_ACTION.host, Action[].class);
+        boolean exists = false;
+        for (Action action : actions)
+            if (TestActions.TEST_ACTION.equals(action)) {
+                exists = true;
+                break;
+            }
+        assertTrue(exists, "Action was added successfully");
+    }
+
+    @Test
+    @Order(2)
+    public void testUpdateAction() throws IOException {
+        TestActions.TEST_ACTION.action = "Destroy";
+        HTTPRequests.put("action", TestActions.TEST_ACTION);
+        // Check if action was updated
+        Action[] actions = HTTPRequests.get("action?host=" + TestActions.TEST_ACTION.host, Action[].class);
+        boolean exists = false;
+        for (Action action : actions)
+            if (TestActions.TEST_ACTION.equals(action)) {
+                exists = true;
+                break;
+            }
+        assertTrue(exists, "Action was updated successfully");
+    }
+
+    @Test
+    @Order(3)
+    public void testDeleteAction() throws IOException {
+        Action deletedAction = HTTPRequests.deleteWithReturn("action", TestActions.TEST_ACTION, Action.class);
+        assertNotNull(deletedAction, "Action was been deleted successfully");
+        // Make sure the action was deleted
+        Action[] actions = HTTPRequests.get("action?action=" + TestActions.TEST_ACTION.action, Action[].class);
+        boolean exists = false;
+        for (Action action : actions)
+            if (TestActions.TEST_ACTION.equals(action)) {
+                exists = true;
+                break;
+            }
+        assertFalse(exists, "Action was successfully deleted");
+    }
+}
