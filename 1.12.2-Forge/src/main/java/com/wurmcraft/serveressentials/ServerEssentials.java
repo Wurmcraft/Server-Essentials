13887bf533c05c7b789ed4be0f3e1ad49b653551
Wurmatron <wurmatron@gmail.com>
2021-06-12 13:19:40 -0400

    Update Build Script, Added Shadow, Spotless, Reflection Lib and Multithreading Libs

--- 1.12.2-Forge/src/main/java/com/wurmcraft/serveressentials/ServerEssentials.java
+++ 1.12.2-Forge/src/main/java/com/wurmcraft/serveressentials/ServerEssentials.java
@@ -8,32 +8,37 @@ import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid = ServerEssentials.MODID, name = ServerEssentials.NAME, version = ServerEssentials.VERSION, dependencies = "", serverSideOnly = true)
+@Mod(
+    modid = ServerEssentials.MODID,
+    name = ServerEssentials.NAME,
+    version = ServerEssentials.VERSION,
+    dependencies = "",
+    serverSideOnly = true)
 public class ServerEssentials {
 
-    public static final String MODID = "server-essentials";
-    public static final String NAME = "Server Essentials";
-    public static final String VERSION = "@VERSION@";
+  public static final String MODID = "server-essentials";
+  public static final String NAME = "Server Essentials";
+  public static final String VERSION = "@VERSION@";
 
-    public static final Logger LOG = LogManager.getLogger("[" + NAME + "]");
+  public static final Logger LOG = LogManager.getLogger("[" + NAME + "]");
 
-    @Mod.EventHandler
-    public void onPreInit(FMLPreInitializationEvent e) {
+  @Mod.EventHandler
+  public void onPreInit(FMLPreInitializationEvent e) {
+    LOG.info("Starting Pre-Initialization");
+  }
 
-    }
+  @Mod.EventHandler
+  public void onInit(FMLInitializationEvent e) {
+    LOG.info("Starting Initialization");
+  }
 
-    @Mod.EventHandler
-    public void onInit(FMLInitializationEvent e) {
+  @Mod.EventHandler
+  public void onPostInit(FMLPostInitializationEvent e) {
+    LOG.info("Starting Post-Initialization");
+  }
 
-    }
-
-    @Mod.EventHandler
-    public void onPostInit(FMLPostInitializationEvent e) {
-
-    }
-
-    @Mod.EventHandler
-    public void onServerStart(FMLServerStartingEvent e) {
-
-    }
+  @Mod.EventHandler
+  public void onServerStart(FMLServerStartingEvent e) {
+    LOG.info("Server Starting has begun");
+  }
 }
