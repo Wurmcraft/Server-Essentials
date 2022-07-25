2e3b03cc87e6a3771bb92dae9f79527241486b8b
Wurmatron <wurmatron@gmail.com>
2022-07-25 00:20:36 -0400

    Fixed Bot Crash when running command via discord dm

--- Backend/src/main/java/io/wurmatron/serveressentials/discord/BotCommands.java
+++ Backend/src/main/java/io/wurmatron/serveressentials/discord/BotCommands.java
@@ -22,17 +22,21 @@ public class BotCommands {
     String username = e.getOption("username")
         .flatMap(ApplicationCommandInteractionOption::getValue)
         .map(ApplicationCommandInteractionOptionValue::asString).get();
-    Member member = e.getInteraction().getMember().get();
-    String discordID = member.getId().asString();
-    String discordName = member.getDisplayName();
-    String verifyCode = EncryptionUtils.generateRandomString(8);
-    LOG.info(
-        "Generating Verify code '" + verifyCode + "' for user '" + username + "' via '"
-            + discordName + "' (" + discordID + ")");
-    verifyCodes.put(username.toUpperCase(),
-        new String[]{discordID, discordName, verifyCode,
-            Instant.now().getEpochSecond() + ""});
-    return e.reply("Type `/verify " + verifyCode + "` in-game to verify")
-        .withEphemeral(true);
+    if (e.getInteraction().getMember().isPresent()) {
+      Member member = e.getInteraction().getMember().get();
+      String discordID = member.getId().asString();
+      String discordName = member.getDisplayName();
+      String verifyCode = EncryptionUtils.generateRandomString(8);
+      LOG.info(
+          "Generating Verify code '" + verifyCode + "' for user '" + username + "' via '"
+              + discordName + "' (" + discordID + ")");
+      verifyCodes.put(username.toUpperCase(),
+          new String[]{discordID, discordName, verifyCode,
+              Instant.now().getEpochSecond() + ""});
+      return e.reply("Type `/verify " + verifyCode + "` in-game to verify")
+          .withEphemeral(true);
+    }
+
+    return e.reply("Unable to find your discord id, please run the command on the discord servers channels.").withEphemeral(true);
   }
 }
