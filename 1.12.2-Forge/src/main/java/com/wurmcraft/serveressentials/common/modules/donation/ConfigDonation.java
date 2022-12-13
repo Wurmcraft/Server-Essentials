525dadef4262c79e6212624186d38f64462f38ab
Wurmatron <wurmatron@gmail.com>
2022-12-13 14:35:01 -0500

    Added Donate Command

--- 1.12.2-Forge/src/main/java/com/wurmcraft/serveressentials/common/modules/donation/ConfigDonation.java
+++ 1.12.2-Forge/src/main/java/com/wurmcraft/serveressentials/common/modules/donation/ConfigDonation.java
@@ -3,4 +3,14 @@ package com.wurmcraft.serveressentials.common.modules.donation;
 import com.wurmcraft.serveressentials.api.loading.ModuleConfig;
 
 @ModuleConfig(module = "Donation")
-public class ConfigDonation {}
+public class ConfigDonation {
+  public String donateURL;
+
+  public ConfigDonation(String donateURL) {
+    this.donateURL = donateURL;
+  }
+
+  public ConfigDonation() {
+    this.donateURL = "https://www.curseforge.com/minecraft/mc-mods/server-essentials";
+  }
+}
---
+++ 1.12.2-Forge/src/main/java/com/wurmcraft/serveressentials/common/modules/donation/command/DonateCommand.java
@@ -0,0 +1,20 @@
+package com.wurmcraft.serveressentials.common.modules.donation.command;
+
+import com.wurmcraft.serveressentials.api.SECore;
+import com.wurmcraft.serveressentials.api.command.Command;
+import com.wurmcraft.serveressentials.api.command.ModuleCommand;
+import com.wurmcraft.serveressentials.api.models.ServerPlayer;
+import com.wurmcraft.serveressentials.common.modules.donation.ConfigDonation;
+import com.wurmcraft.serveressentials.common.utils.ChatHelper;
+import net.minecraft.util.text.TextFormatting;
+
+@ModuleCommand(module = "Donate", name = "Donate", defaultAliases = "Support")
+public class DonateCommand {
+
+  @Command(args = {}, usage = {}, canConsoleUse = true)
+  public void donate(ServerPlayer player) {
+    ChatHelper.send(player.sender,
+        TextFormatting.AQUA + ((ConfigDonation) SECore.moduleConfigs.get(
+            "DONATE")).donateURL);
+  }
+}
