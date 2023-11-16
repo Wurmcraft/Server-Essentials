87484f8feb2539debf2f43ae1d776d82be3c97cc
Wurmatron <wurmatron@wurmcraft.com>
2023-11-16 10:53:50 -0500

    Added config option `displayUUIDOnHover` to Display UUID on hover, to see who sent it. Turned off by default.

--- 1.12.2-Forge/src/main/java/com/wurmcraft/serveressentials/common/modules/chat/ConfigChat.java
+++ 1.12.2-Forge/src/main/java/com/wurmcraft/serveressentials/common/modules/chat/ConfigChat.java
@@ -11,22 +11,17 @@ public class ConfigChat {
   public String messageFormat;
   public String defaultMuteDuration;
   public String[] motd;
+  public boolean displayUUIDOnHover;
 
-  public ConfigChat(
-      String defaultChannel,
-      String chatFormat,
-      String nickFormat,
-      String messageFormat,
-      String defaultMuteDuration,
-      String[] motd) {
+  public ConfigChat(String defaultChannel, String defaultChatFormat, String nickFormat, String messageFormat, String defaultMuteDuration, String[] motd, boolean displayUUIDOnHover) {
     this.defaultChannel = defaultChannel;
-    this.defaultChatFormat = chatFormat;
+    this.defaultChatFormat = defaultChatFormat;
     this.nickFormat = nickFormat;
     this.messageFormat = messageFormat;
     this.defaultMuteDuration = defaultMuteDuration;
     this.motd = motd;
+    this.displayUUIDOnHover = displayUUIDOnHover;
   }
-
   public ConfigChat() {
     this.defaultChannel = "local";
     this.defaultChatFormat = "%CHANNEL_PREFIX% %RANK_PREFIX% %NAME% %RANK_SUFFIX%: %MESSAGE%";
@@ -35,5 +30,6 @@ public class ConfigChat {
     this.messageFormat = "%NAME% {->, <-} %MSG%";
     this.motd =
         new String[] {"&bWelcome %NAME%,", "&bThis server is running with Server Essentials"};
+    this.displayUUIDOnHover = false;
   }
 }
