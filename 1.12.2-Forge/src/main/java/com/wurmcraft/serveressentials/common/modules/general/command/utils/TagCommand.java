bfa8fa1c80cd6a17af60485b26568f3e42ae9dcc
Wurmatron <wurmatron@wurmcraft.com>
2023-11-18 20:36:20 -0500

    Fixed FileDataLoader not caching correctly, Fixed tag command not formatting correctly. Fixed minor crash related to General Events

--- 1.12.2-Forge/src/main/java/com/wurmcraft/serveressentials/common/modules/general/command/utils/TagCommand.java
+++ 1.12.2-Forge/src/main/java/com/wurmcraft/serveressentials/common/modules/general/command/utils/TagCommand.java
@@ -11,20 +11,49 @@ import net.minecraft.entity.player.EntityPlayer;
 @ModuleCommand(module = "General", name = "Tag")
 public class TagCommand {
 
-    @Command(args = {CommandArgument.PLAYER, CommandArgument.STRING}, usage = {"player", "tag"}, isSubCommand = true, canConsoleUse = true, subCommandAliases = {"A"})
-    public void add(ServerPlayer sender, EntityPlayer player, String tag) {
-        player.addTag(tag);
-        ChatHelper.send(sender.sender, sender.lang.COMMAND_TAG_ADD.replaceAll("\\{@TAG@}", tag).replaceAll("\\{@PLAYER@}", player.getName()));
-    }
+  @Command(
+      args = {CommandArgument.PLAYER, CommandArgument.STRING},
+      usage = {"player", "tag"},
+      isSubCommand = true,
+      canConsoleUse = true,
+      subCommandAliases = {"A"})
+  public void add(ServerPlayer sender, EntityPlayer player, String tag) {
+    player.addTag(tag);
+    ChatHelper.send(
+        sender.sender,
+        sender
+            .lang
+            .COMMAND_TAG_ADD
+            .replaceAll("\\{@TAG@}", tag)
+            .replaceAll("\\{@PLAYER@}", player.getName()));
+  }
 
-    @Command(args = {CommandArgument.PLAYER, CommandArgument.STRING}, usage = {"player", "tag"}, isSubCommand = true, canConsoleUse = true, subCommandAliases = {"Rem", "R", "Delete", "Del", "D"})
-    public void remove(ServerPlayer sender, EntityPlayer player, String tag) {
-        player.removeTag(tag);
-        ChatHelper.send(sender.sender, sender.lang.COMMAND_TAG_REM.replaceAll("\\{@TAG@}", tag).replaceAll("\\{@PLAYER@}", player.getName()));
-    }
+  @Command(
+      args = {CommandArgument.PLAYER, CommandArgument.STRING},
+      usage = {"player", "tag"},
+      isSubCommand = true,
+      canConsoleUse = true,
+      subCommandAliases = {"Rem", "R", "Delete", "Del", "D"})
+  public void remove(ServerPlayer sender, EntityPlayer player, String tag) {
+    player.removeTag(tag);
+    ChatHelper.send(
+        sender.sender,
+        sender
+            .lang
+            .COMMAND_TAG_REM
+            .replaceAll("\\{@TAG@}", tag)
+            .replaceAll("\\{@PLAYER@}", player.getName()));
+  }
 
-    @Command(args = {CommandArgument.PLAYER}, usage = {"player"}, isSubCommand = true, canConsoleUse = true, subCommandAliases = {"l"})
-    public void list(ServerPlayer sender, EntityPlayer player) {
-        ChatHelper.send(sender.sender, sender.lang.COMMAND_TAG_LIST.replaceAll("\\{@LIST@}", Strings.join(player.getTags(), ",")));
-    }
+  @Command(
+      args = {CommandArgument.PLAYER},
+      usage = {"player"},
+      isSubCommand = true,
+      canConsoleUse = true,
+      subCommandAliases = {"l"})
+  public void list(ServerPlayer sender, EntityPlayer player) {
+    ChatHelper.send(
+        sender.sender,
+        sender.lang.COMMAND_TAG_LIST.replaceAll("\\{@LIST@}", Strings.join(player.getTags(), ",")));
+  }
 }
