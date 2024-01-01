03763c07b3cc334764b5d98578806c024a54c1a0
Wurmatron <wurmatron@wurmcraft.com>
2024-01-01 13:47:26 -0500

    Fixed Eco Command and Eco Util's from glitching when missing a currency

--- 1.12.2-Forge/src/main/java/com/wurmcraft/serveressentials/common/command/EcoUtils.java
+++ 1.12.2-Forge/src/main/java/com/wurmcraft/serveressentials/common/command/EcoUtils.java
@@ -7,72 +7,89 @@ import com.wurmcraft.serveressentials.common.data.loader.DataLoader.DataType;
 import com.wurmcraft.serveressentials.common.modules.economy.ConfigEconomy;
 import com.wurmcraft.serveressentials.common.modules.economy.ConfigEconomy.Taxes;
 
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class EcoUtils {
 
-  /**
-   * Checks if the provided account can purchase with the provided amount
-   *
-   * @param currency name of the currency
-   * @param amount amount of the currency
-   * @param user account to find the currency from
-   */
-  public static boolean canBuy(String currency, double amount, Account user) {
-    if (user != null && user.wallet != null) {
-      for (BankAccount account : user.wallet) {
-        if (account.currencyName.equalsIgnoreCase(currency)) {
-          return account.amount >= amount;
+    /**
+     * Checks if the provided account can purchase with the provided amount
+     *
+     * @param currency name of the currency
+     * @param amount   amount of the currency
+     * @param user     account to find the currency from
+     */
+    public static boolean canBuy(String currency, double amount, Account user) {
+        if (user != null && user.wallet != null) {
+            for (BankAccount account : user.wallet) {
+                if (account.currencyName.equalsIgnoreCase(currency)) {
+                    return account.amount >= amount;
+                }
+            }
         }
-      }
+        return false;
     }
-    return false;
-  }
 
-  public static double balance(Account account, String currency) {
-    if (account.wallet != null && account.wallet.length > 0) {
-      for (BankAccount bank : account.wallet) {
-        if (bank.currencyName.equalsIgnoreCase(currency)) {
-          return bank.amount;
+    public static double balance(Account account, String currency) {
+        if (account.wallet != null && account.wallet.length > 0) {
+            for (BankAccount bank : account.wallet) {
+                if (bank.currencyName.equalsIgnoreCase(currency)) {
+                    return bank.amount;
+                }
+            }
         }
-      }
+        return 0.00;
     }
-    return 0.00;
-  }
 
-  public static void buy(Account account, String currency, double amount) {
-    for (BankAccount bank : account.wallet) {
-      if (bank.currencyName.equals(currency)) {
-        bank.amount = bank.amount - amount;
+    public static void buy(Account account, String currency, double amount) {
+        for (BankAccount bank : account.wallet) {
+            if (bank.currencyName.equals(currency)) {
+                bank.amount = bank.amount - amount;
+                SECore.dataLoader.update(DataType.ACCOUNT, account.uuid, account);
+                return;
+            }
+        }
+    }
+
+    public static void earn(Account account, String currency, double amount) {
+        for (BankAccount bank : account.wallet) {
+            if (bank.currencyName.equals(currency)) {
+                bank.amount = bank.amount + amount;
+                SECore.dataLoader.update(DataType.ACCOUNT, account.uuid, account);
+                return;
+            }
+        }
+        BankAccount newAccount = new BankAccount(currency, amount, Instant.now().getEpochSecond(), "Basic", "{}", Instant.now().getEpochSecond());
+        List<BankAccount> accounts = new ArrayList<>();
+        Collections.addAll(accounts, account.wallet);
+        accounts.add(newAccount);
+        account.wallet = accounts.toArray(new BankAccount[0]);
         SECore.dataLoader.update(DataType.ACCOUNT, account.uuid, account);
-        return;
-      }
     }
-  }
 
-  public static void earn(Account account, String currency, double amount) {
-    for (BankAccount bank : account.wallet) {
-      if (bank.currencyName.equals(currency)) {
-        bank.amount = bank.amount + amount;
+    public static double computeTax(double amount) {
+        Taxes taxes = ((ConfigEconomy) SECore.moduleConfigs.get("ECONOMY")).taxes;
+        if (taxes.payTaxMultiplayer > 0) {
+            return amount + (amount * taxes.payTaxMultiplayer);
+        }
+        return amount;
+    }
+
+    public static void set(Account account, String currency, double amount) {
+        for (BankAccount bank : account.wallet) {
+            if (bank.currencyName.equals(currency)) {
+                bank.amount = amount;
+                SECore.dataLoader.update(DataType.ACCOUNT, account.uuid, account);
+                return;
+            }
+        }
+        BankAccount newAccount = new BankAccount(currency, amount, Instant.now().getEpochSecond(), "Basic", "{}", Instant.now().getEpochSecond());
+        List<BankAccount> accounts = new ArrayList<>();
+        Collections.addAll(accounts, account.wallet);
+        accounts.add(newAccount);
+        account.wallet = accounts.toArray(new BankAccount[0]);
         SECore.dataLoader.update(DataType.ACCOUNT, account.uuid, account);
-        return;
-      }
     }
-  }
-
-  public static double computeTax(double amount) {
-    Taxes taxes = ((ConfigEconomy) SECore.moduleConfigs.get("ECONOMY")).taxes;
-    if (taxes.payTaxMultiplayer > 0) {
-      return amount + (amount * taxes.payTaxMultiplayer);
-    }
-    return amount;
-  }
-
-  public static void set(Account account, String currency, double amount) {
-    for (BankAccount bank : account.wallet) {
-      if (bank.currencyName.equals(currency)) {
-        bank.amount = amount;
-        SECore.dataLoader.update(DataType.ACCOUNT, account.uuid, account);
-        return;
-      }
-    }
-  }
 }
--- 1.12.2-Forge/src/main/java/com/wurmcraft/serveressentials/common/modules/economy/command/EcoCommand.java
+++ 1.12.2-Forge/src/main/java/com/wurmcraft/serveressentials/common/modules/economy/command/EcoCommand.java
@@ -17,196 +17,132 @@ import net.minecraft.entity.player.EntityPlayer;
 @ModuleCommand(module = "Economy", name = "Eco")
 public class EcoCommand {
 
-    @Command(
-            args = {CommandArgument.DOUBLE},
-            usage = {"amount"},
-            isSubCommand = true,
-            subCommandAliases = "S",
-            canConsoleUse = false)
-    public void Set(ServerPlayer player, double amount) {
-        Set(player, player.player, amount);
-    }
-
-    @Command(
-            args = {CommandArgument.PLAYER, CommandArgument.DOUBLE},
-            usage = {"player", "amount"},
-            isSubCommand = true,
-            subCommandAliases = "S",
-            canConsoleUse = true)
-    public void Set(ServerPlayer player, EntityPlayer otherPlayer, double amount) {
-        Set(
-                player,
-                player.player,
-                ((ConfigEconomy) SECore.moduleConfigs.get("ECONOMY")).serverCurrency,
-                amount);
-    }
-
-    @Command(
-            args = {CommandArgument.PLAYER, CommandArgument.CURRENCY, CommandArgument.DOUBLE},
-            usage = {"player", "currency", "amount"},
-            isSubCommand = true,
-            subCommandAliases = "S",
-            canConsoleUse = true)
-    public void Set(ServerPlayer player, EntityPlayer otherPlayer, String currency, double amount) {
-        Account otherAccount =
-                PlayerUtils.getLatestAccount(otherPlayer.getGameProfile().getId().toString());
-        if (otherAccount != null) {
-            EcoUtils.set(otherAccount, currency, amount);
-            // Display
-            if (player.player.getGameProfile().getId().toString().equals(otherAccount.uuid)) {
-                ChatHelper.send(
-                        player.sender,
-                        player
-                                .lang
-                                .COMMAND_ECO_SET
-                                .replaceAll("\\{@AMOUNT@\\}", String.format("%.2f", amount))
-                                .replaceAll("\\{@CURRENCY@\\}", currency));
-            } else {
-                ChatHelper.send(
-                        otherPlayer,
-                        SECore.dataLoader
-                                .get(DataLoader.DataType.LANGUAGE, otherAccount.lang, new Language())
-                                .COMMAND_ECO_SET
-                                .replaceAll("\\{@AMOUNT@\\}", String.format("%.2f", amount))
-                                .replaceAll("\\{@CURRENCY@\\}", currency));
-                ChatHelper.send(
-                        player.sender,
-                        player
-                                .lang
-                                .COMMAND_ECO_SET_OTHER
-                                .replaceAll("\\{@AMOUNT@\\}", String.format("%.2f", amount))
-                                .replaceAll("\\{@CURRENCY@\\}", currency)
-                                .replaceAll("\\{@PLAYER@\\}", ChatHelper.getName(otherPlayer, otherAccount)));
-            }
+    @Command(args = {CommandArgument.STRING, CommandArgument.DOUBLE}, usage = {"Set,Add,Consume", "amount"}, canConsoleUse = false)
+    public void ecoCommand(ServerPlayer player, String type, double amount) {
+        if (type.equalsIgnoreCase("Set") || type.equalsIgnoreCase("S")) {
+            ecoCommand(player, "Set", player.player, amount, ((ConfigEconomy) SECore.moduleConfigs.get("ECONOMY")).serverCurrency);
+        } else if (type.equalsIgnoreCase("Add") || type.equalsIgnoreCase("A")) {
+            ecoCommand(player, "Add", player.player, amount, ((ConfigEconomy) SECore.moduleConfigs.get("ECONOMY")).serverCurrency);
+        } else if (type.equalsIgnoreCase("Consume") || type.equalsIgnoreCase("Cons") || type.equalsIgnoreCase("C") | type.equalsIgnoreCase("Eat") || type.equalsIgnoreCase("E")) {
+            ecoCommand(player, "Consume", player.player, amount, ((ConfigEconomy) SECore.moduleConfigs.get("ECONOMY")).serverCurrency);
+        } else {
+            ChatHelper.send(player.sender, "Set, Add, Consume");
         }
     }
 
-    @Command(
-            args = {CommandArgument.DOUBLE},
-            usage = {"amount"},
-            isSubCommand = true,
-            subCommandAliases = "A",
-            canConsoleUse = false)
-    public void Add(ServerPlayer player, double amount) {
-        Add(player, player.player, amount);
-    }
-
-    @Command(
-            args = {CommandArgument.PLAYER, CommandArgument.DOUBLE},
-            usage = {"player", "amount"},
-            isSubCommand = true,
-            subCommandAliases = "A",
-            canConsoleUse = true)
-    public void Add(ServerPlayer player, EntityPlayer otherPlayer, double amount) {
-        Add(
-                player,
-                otherPlayer,
-                ((ConfigEconomy) SECore.moduleConfigs.get("ECONOMY")).serverCurrency,
-                amount);
-    }
-
-    @Command(
-            args = {CommandArgument.PLAYER, CommandArgument.CURRENCY, CommandArgument.DOUBLE},
-            usage = {"player", "currency", "amount"},
-            isSubCommand = true,
-            subCommandAliases = "A",
-            canConsoleUse = true)
-    public void Add(ServerPlayer player, EntityPlayer otherPlayer, String currency, double amount) {
-        Account otherAccount =
-                PlayerUtils.getLatestAccount(otherPlayer.getGameProfile().getId().toString());
-        if (otherAccount != null) {
-            EcoUtils.earn(otherAccount, currency, amount);
-            // Display
-            if (player.player.getGameProfile().getId().toString().equals(otherAccount.uuid)) {
-                ChatHelper.send(
-                        player.sender,
-                        player
-                                .lang
-                                .COMMAND_ECO_ADD
-                                .replaceAll("\\{@AMOUNT@\\}", String.format("%.2f", amount))
-                                .replaceAll("\\{@CURRENCY@\\}", currency));
-            } else {
-                ChatHelper.send(
-                        otherPlayer,
-                        SECore.dataLoader
-                                .get(DataLoader.DataType.LANGUAGE, otherAccount.lang, new Language())
-                                .COMMAND_ECO_SET
-                                .replaceAll("\\{@AMOUNT@\\}", String.format("%.2f", amount))
-                                .replaceAll("\\{@CURRENCY@\\}", currency));
-                ChatHelper.send(
-                        player.sender,
-                        player
-                                .lang
-                                .COMMAND_ECO_ADD_OTHER
-                                .replaceAll("\\{@AMOUNT@\\}", String.format("%.2f", amount))
-                                .replaceAll("\\{@CURRENCY@\\}", currency)
-                                .replaceAll("\\{@PLAYER@\\}", ChatHelper.getName(otherPlayer, otherAccount)));
-            }
+    @Command(args = {CommandArgument.STRING, CommandArgument.PLAYER, CommandArgument.DOUBLE}, usage = {"Set,Add,Consume", "player", "amount"}, canConsoleUse = true)
+    public void ecoCommand(ServerPlayer player, String type, EntityPlayer otherPlayer, double amount) {
+        if (type.equalsIgnoreCase("Set") || type.equalsIgnoreCase("S")) {
+            ecoCommand(player, "Set", otherPlayer, amount, ((ConfigEconomy) SECore.moduleConfigs.get("ECONOMY")).serverCurrency);
+        } else if (type.equalsIgnoreCase("Add") || type.equalsIgnoreCase("A")) {
+            ecoCommand(player, "Add", otherPlayer, amount, ((ConfigEconomy) SECore.moduleConfigs.get("ECONOMY")).serverCurrency);
+        } else if (type.equalsIgnoreCase("Consume") || type.equalsIgnoreCase("Cons") || type.equalsIgnoreCase("C") | type.equalsIgnoreCase("Eat") || type.equalsIgnoreCase("E")) {
+            ecoCommand(player, "Consume", otherPlayer, amount, ((ConfigEconomy) SECore.moduleConfigs.get("ECONOMY")).serverCurrency);
+        } else {
+            ChatHelper.send(player.sender, "Set, Add, Consume");
         }
     }
 
-    @Command(
-            args = {CommandArgument.DOUBLE},
-            usage = {"amount"},
-            isSubCommand = true,
-            subCommandAliases = {"C", "Con", "Eat", "E"},
-            canConsoleUse = false)
-    public void consume(ServerPlayer player, double amount) {
-        consumeP(player, player.player, amount);
-    }
-
-    @Command(
-            args = {CommandArgument.PLAYER, CommandArgument.DOUBLE},
-            usage = {"player", "amount"},
-            isSubCommand = true,
-            subCommandAliases = {"C", "Con", "Eat", "E"},
-            canConsoleUse = true)
-    public void consumeP(ServerPlayer player, EntityPlayer otherPlayer, double amount) {
-        consumeC(
-                player,
-                otherPlayer,
-                amount,
-                ((ConfigEconomy) SECore.moduleConfigs.get("ECONOMY")).serverCurrency);
-    }
-
-    @Command(
-            args = {CommandArgument.PLAYER, CommandArgument.DOUBLE, CommandArgument.CURRENCY},
-            usage = {"player", "amount", "currency"},
-            isSubCommand = true,
-            subCommandAliases = {"C", "Con", "Eat", "E"},
-            canConsoleUse = true)
-    public void consumeC(
-            ServerPlayer player, EntityPlayer otherPlayer, double amount, String currency) {
-        Account otherAccount =
-                PlayerUtils.getLatestAccount(otherPlayer.getGameProfile().getId().toString());
-        if (otherAccount != null) {
-            EcoUtils.buy(otherAccount, currency, amount);
-            // Display
-            if (player.player.getGameProfile().getId().toString().equals(otherAccount.uuid)) {
-                ChatHelper.send(
-                        player.sender,
-                        player
-                                .lang
-                                .COMMAND_ECO_CONSUME
-                                .replaceAll("\\{@AMOUNT@\\}", String.format("%.2f", amount))
-                                .replaceAll("\\{@CURRENCY@\\}", currency));
-            } else {
-                ChatHelper.send(
-                        otherPlayer,
-                        SECore.dataLoader
-                                .get(DataLoader.DataType.LANGUAGE, otherAccount.lang, new Language())
-                                .COMMAND_ECO_SET
-                                .replaceAll("\\{@AMOUNT@\\}", String.format("%.2f", amount))
-                                .replaceAll("\\{@CURRENCY@\\}", currency));
-                ChatHelper.send(
-                        player.sender,
-                        player
-                                .lang
-                                .COMMAND_ECO_CONSUME_OTHER
-                                .replaceAll("\\{@AMOUNT@\\}", String.format("%.2f", amount))
-                                .replaceAll("\\{@CURRENCY@\\}", currency)
-                                .replaceAll("\\{@PLAYER@\\}", ChatHelper.getName(otherPlayer, otherAccount)));
+    @Command(args = {CommandArgument.STRING, CommandArgument.PLAYER, CommandArgument.DOUBLE, CommandArgument.CURRENCY}, usage = {"Set,Add,Consume", "player", "amount", "currency"}, canConsoleUse = true)
+    public void ecoCommand(ServerPlayer player, String type, EntityPlayer otherPlayer, double amount, String currency) {
+        if (type.equalsIgnoreCase("Set") || type.equalsIgnoreCase("S")) {
+            Account otherAccount =
+                    PlayerUtils.getLatestAccount(otherPlayer.getGameProfile().getId().toString());
+            if (otherAccount != null) {
+                EcoUtils.set(otherAccount, currency, amount);
+                // Display
+                if (player.player.getGameProfile().getId().toString().equals(otherAccount.uuid)) {
+                    ChatHelper.send(
+                            player.sender,
+                            player
+                                    .lang
+                                    .COMMAND_ECO_SET
+                                    .replaceAll("\\{@AMOUNT@\\}", String.format("%.2f", amount))
+                                    .replaceAll("\\{@CURRENCY@\\}", currency));
+                } else {
+                    ChatHelper.send(
+                            otherPlayer,
+                            SECore.dataLoader
+                                    .get(DataLoader.DataType.LANGUAGE, otherAccount.lang, new Language())
+                                    .COMMAND_ECO_SET
+                                    .replaceAll("\\{@AMOUNT@\\}", String.format("%.2f", amount))
+                                    .replaceAll("\\{@CURRENCY@\\}", currency));
+                    ChatHelper.send(
+                            player.sender,
+                            player
+                                    .lang
+                                    .COMMAND_ECO_SET_OTHER
+                                    .replaceAll("\\{@AMOUNT@\\}", String.format("%.2f", amount))
+                                    .replaceAll("\\{@CURRENCY@\\}", currency)
+                                    .replaceAll("\\{@PLAYER@\\}", ChatHelper.getName(otherPlayer, otherAccount)));
+                }
             }
+        } else if (type.equalsIgnoreCase("Add") || type.equalsIgnoreCase("A")) {
+            Account otherAccount =
+                    PlayerUtils.getLatestAccount(otherPlayer.getGameProfile().getId().toString());
+            if (otherAccount != null) {
+                EcoUtils.earn(otherAccount, currency, amount);
+                // Display
+                if (player.player.getGameProfile().getId().toString().equals(otherAccount.uuid)) {
+                    ChatHelper.send(
+                            player.sender,
+                            player
+                                    .lang
+                                    .COMMAND_ECO_ADD
+                                    .replaceAll("\\{@AMOUNT@\\}", String.format("%.2f", amount))
+                                    .replaceAll("\\{@CURRENCY@\\}", currency));
+                } else {
+                    ChatHelper.send(
+                            otherPlayer,
+                            SECore.dataLoader
+                                    .get(DataLoader.DataType.LANGUAGE, otherAccount.lang, new Language())
+                                    .COMMAND_ECO_SET
+                                    .replaceAll("\\{@AMOUNT@\\}", String.format("%.2f", amount))
+                                    .replaceAll("\\{@CURRENCY@\\}", currency));
+                    ChatHelper.send(
+                            player.sender,
+                            player
+                                    .lang
+                                    .COMMAND_ECO_ADD_OTHER
+                                    .replaceAll("\\{@AMOUNT@\\}", String.format("%.2f", amount))
+                                    .replaceAll("\\{@CURRENCY@\\}", currency)
+                                    .replaceAll("\\{@PLAYER@\\}", ChatHelper.getName(otherPlayer, otherAccount)));
+                }
+            }
+        } else if (type.equalsIgnoreCase("Consume") || type.equalsIgnoreCase("Cons") || type.equalsIgnoreCase("C") | type.equalsIgnoreCase("Eat") || type.equalsIgnoreCase("E")) {
+            Account otherAccount =
+                    PlayerUtils.getLatestAccount(otherPlayer.getGameProfile().getId().toString());
+            if (otherAccount != null) {
+                EcoUtils.buy(otherAccount, currency, amount);
+                // Display
+                if (player.player.getGameProfile().getId().toString().equals(otherAccount.uuid)) {
+                    ChatHelper.send(
+                            player.sender,
+                            player
+                                    .lang
+                                    .COMMAND_ECO_CONSUME
+                                    .replaceAll("\\{@AMOUNT@\\}", String.format("%.2f", amount))
+                                    .replaceAll("\\{@CURRENCY@\\}", currency));
+                } else {
+                    ChatHelper.send(
+                            otherPlayer,
+                            SECore.dataLoader
+                                    .get(DataLoader.DataType.LANGUAGE, otherAccount.lang, new Language())
+                                    .COMMAND_ECO_SET
+                                    .replaceAll("\\{@AMOUNT@\\}", String.format("%.2f", amount))
+                                    .replaceAll("\\{@CURRENCY@\\}", currency));
+                    ChatHelper.send(
+                            player.sender,
+                            player
+                                    .lang
+                                    .COMMAND_ECO_CONSUME_OTHER
+                                    .replaceAll("\\{@AMOUNT@\\}", String.format("%.2f", amount))
+                                    .replaceAll("\\{@CURRENCY@\\}", currency)
+                                    .replaceAll("\\{@PLAYER@\\}", ChatHelper.getName(otherPlayer, otherAccount)));
+                }
+            }
+        } else {
+            ChatHelper.send(player.sender, "Set, Add, Consume");
         }
     }
 }
