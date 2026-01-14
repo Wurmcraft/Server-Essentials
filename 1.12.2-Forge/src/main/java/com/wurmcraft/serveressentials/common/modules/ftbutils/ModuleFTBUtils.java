278cf053ebe759ac24cd584fcaab65c1d09741c9
Wurmatron <wurmatron@wurmcraft.com>
2026-01-13 20:47:35 -0500

    Work on FTBUtils Support

--- 1.12.2-Forge/src/main/java/com/wurmcraft/serveressentials/common/modules/ftbutils/ModuleFTBUtils.java
+++ 1.12.2-Forge/src/main/java/com/wurmcraft/serveressentials/common/modules/ftbutils/ModuleFTBUtils.java
@@ -1,12 +1,27 @@
 package com.wurmcraft.serveressentials.common.modules.ftbutils;
 
+import com.wurmcraft.serveressentials.ServerEssentials;
 import com.wurmcraft.serveressentials.api.loading.Module;
+import com.wurmcraft.serveressentials.common.modules.ftbutils.event.FTBUtilsEvents;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Loader;
 
-// TODO Implement
+import java.io.File;
+
+
 @Module(name = "FTBUtils")
 public class ModuleFTBUtils {
 
-  public void setup() {}
+  public static File PLAYER_RANKS = new File(
+          "local" + File.separator + "ftbutilities" + File.separator + "players.txt");
+
+  public void setup() {
+    if (Loader.isModLoaded("ftbutilities")) {
+      MinecraftForge.EVENT_BUS.register(new FTBUtilsEvents());
+    } else {
+      ServerEssentials.LOG.warn("FTBUTILS Module is disabled, FTBUtils is not found");
+    }
+  }
 
   public void reload() {}
 }
---
+++ 1.12.2-Forge/src/main/java/com/wurmcraft/serveressentials/common/modules/ftbutils/event/FTBUtilsEvents.java
@@ -0,0 +1,35 @@
+package com.wurmcraft.serveressentials.common.modules.ftbutils.event;
+
+import com.wurmcraft.serveressentials.api.SECore;
+import com.wurmcraft.serveressentials.api.event.PlayerLoadEvent;
+import com.wurmcraft.serveressentials.api.models.Account;
+import com.wurmcraft.serveressentials.api.models.Rank;
+import com.wurmcraft.serveressentials.common.data.loader.DataLoader;
+import com.wurmcraft.serveressentials.common.modules.ftbutils.FtbUtilsUtils;
+import com.wurmcraft.serveressentials.common.utils.PlayerUtils;
+import net.minecraftforge.fml.common.eventhandler.EventPriority;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.PlayerEvent;
+
+public class FTBUtilsEvents {
+
+
+    @SubscribeEvent(priority = EventPriority.LOW)
+    public void newPlayer(PlayerLoadEvent e) {
+        if (e.newAccount) {
+            FtbUtilsUtils.setPlayerClaimBlocks(e.player.getGameProfile().getId().toString(),
+                    FtbUtilsUtils.get(e.account, PlayerUtils.getUserRanks(e.account).toArray(new Rank[0]), new String[]{"ftbutils.claim.", "claimblocks.amount."}),
+                    FtbUtilsUtils.get(e.account, PlayerUtils.getUserRanks(e.account).toArray(new Rank[0]), new String[]{"ftbutils.chunkloading.", "chunkloading.amount."}));
+        }
+    }
+
+    @SubscribeEvent(priority = EventPriority.LOW)
+    public void onPlayerLogin(PlayerEvent.PlayerLoggedInEvent e) {
+        Account account = SECore.dataLoader.get(DataLoader.DataType.ACCOUNT, e.player.getGameProfile().getId().toString(), new Account());
+        if (account != null && account.rank != null) {
+            FtbUtilsUtils.updatePlayerClaimBlocks(e.player,
+                    FtbUtilsUtils.get(account, PlayerUtils.getUserRanks(account).toArray(new Rank[0]), new String[]{"ftbutils.claim.", "claimblocks.amount."}),
+                    FtbUtilsUtils.get(account, PlayerUtils.getUserRanks(account).toArray(new Rank[0]), new String[]{"ftbutils.chunkloading.", "chunkloading.amount."}));
+        }
+    }
+}
