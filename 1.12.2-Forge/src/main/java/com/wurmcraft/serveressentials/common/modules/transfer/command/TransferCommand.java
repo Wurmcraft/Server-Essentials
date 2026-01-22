14f9740e984e6259286ebd643553fd8434d74657
Wurmatron <wurmatron@wurmcraft.com>
2026-01-21 21:45:38 -0500

    More work on transfer command. (Basic entry creation)

--- 1.12.2-Forge/src/main/java/com/wurmcraft/serveressentials/common/modules/transfer/command/TransferCommand.java
+++ 1.12.2-Forge/src/main/java/com/wurmcraft/serveressentials/common/modules/transfer/command/TransferCommand.java
@@ -1,14 +1,22 @@
 package com.wurmcraft.serveressentials.common.modules.transfer.command;
 
+import com.wurmcraft.serveressentials.ServerEssentials;
 import com.wurmcraft.serveressentials.api.SECore;
 import com.wurmcraft.serveressentials.api.command.Command;
 import com.wurmcraft.serveressentials.api.command.CommandArgument;
 import com.wurmcraft.serveressentials.api.command.ModuleCommand;
 import com.wurmcraft.serveressentials.api.models.ServerPlayer;
 import com.wurmcraft.serveressentials.api.models.TransferEntry;
+import com.wurmcraft.serveressentials.api.models.transfer.ItemWrapper;
+import com.wurmcraft.serveressentials.common.data.loader.DataLoader;
 import com.wurmcraft.serveressentials.common.modules.general.utils.inventory.TransferInventory;
 import com.wurmcraft.serveressentials.common.modules.transfer.ConfigTransfer;
 import com.wurmcraft.serveressentials.common.modules.transfer.utils.TransferUtils;
+import net.minecraft.entity.player.InventoryPlayer;
+import net.minecraft.item.ItemStack;
+
+import java.time.Instant;
+import java.util.Arrays;
 
 
 @ModuleCommand(module = "Transfer", name = "Transfer", defaultAliases = {"tsfr", "tfr", "tf"})
@@ -18,7 +26,27 @@ public class TransferCommand {
 
     @Command(args = {CommandArgument.STRING}, usage = {"all,hand,hotbar"}, isSubCommand = true, subCommandAliases = {"s"})
     public static void send(ServerPlayer sender, String type) {
-        // TODO Implement
+        try {
+            // TODO Entry Exists
+        } catch (Exception e) {
+            ItemStack[] items = null;
+            InventoryPlayer pInv = sender.player.inventory;
+            if(type.equalsIgnoreCase("hand") || type.equalsIgnoreCase("h")) {
+                items = new ItemStack[] {pInv.getCurrentItem()};
+            } else if(type.equalsIgnoreCase("hotbar") || type.equalsIgnoreCase("hot") || type.equalsIgnoreCase("bar") || type.equalsIgnoreCase("hb")) {
+                items = new ItemStack[9];
+                for(int index = 0; index < 9; index++)
+                    items[index] = pInv.getStackInSlot(index);
+            } else if(type.equalsIgnoreCase("all") || type.equalsIgnoreCase("*") | type.equalsIgnoreCase("a")) {
+                items = new ItemStack[36];
+                for(int index = 0; index < 36; index++)
+                    items[index] = pInv.getStackInSlot(index);
+            }
+            TransferEntry entry = new TransferEntry(-1,sender.player.getGameProfile().getId().toString(), Instant.now().getEpochSecond(), convert(items), TRANSFER_ID,false);
+            SECore.dataLoader.register(DataLoader.DataType.TRANSFER,TRANSFER_ID,entry);
+            // TODO Delete Items
+            // ChatHelper.send();
+        }
     }
 
     @Command(args = {}, usage = {}, isSubCommand = true, subCommandAliases = {"g"})
@@ -42,5 +70,9 @@ public class TransferCommand {
             // TODO does not exist
 //            ChatHelper.send();
         }
+    }
+
+    public static ItemWrapper[] convert(ItemStack[] items) {
+        return Arrays.stream(items).map(item -> new ItemWrapper(ServerEssentials.stackConverter.toString(item))).toArray(ItemWrapper[]::new);
     }
 }
